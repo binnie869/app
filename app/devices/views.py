4e2a5c72684e9fae8299374489d37d87004d5b36
AnubhavGupta <anubhav.rg@berkeley.edu>
2016-03-25 14:55:32 -0700

    Adding functionlity to add device to DB and fixing the login and signup form positioning

--- app/devices/views.py
+++ app/devices/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app import db, login_manager, pubnub
 from flask.ext.login import login_required, current_user
 from app.auth.models import User
@@ -19,7 +19,29 @@ def list_devices():
 @mod_devices.route('/add_device', methods=['POST'])
 @login_required
 def add_device():
-	pass
+	username = current_user.get_id()
+	print(username)
+	existing_device = db.devices.find_one({'name' :
+                                           request.form['name']})
+	if existing_device:
+		device_list = []
+		devices = db.devices.find({'username': current_user.get_id()})
+		for device in devices:
+			device_list.append((device['name'], device['type'], \
+					device['sensors'], device['actuators'], device['setup']))
+		return render_template('devices/devices_m.html',
+	                          title='Your Devices', my_devices=device_list)
+	else:
+		new_device = {'username' : username, 'name' : request.form['name'], 'type' : request.form['type'], 'setup' : request.form['setup'], 'actuators' : [], 'sensors' : []}
+		db.devices.insert_one(new_device)
+		device_list = []
+		devices = db.devices.find({'username': current_user.get_id()})
+		for device in devices:
+			device_list.append((device['name'], device['type'], \
+					device['sensors'], device['actuators'], device['setup']))
+		return render_template('devices/devices_m.html',
+	                           title='Your Devices', my_devices=device_list)
+
 
 @mod_devices.route('/modify_device', methods=['POST'])
 @login_required
